113803b8f743e27b612d324db1fd57870e197b17
Troll321 <atila.ghulwani@gmail.com>
2025-10-21 08:39:25 +0700

    Fix Rest /summary API and lib filterMongo

--- lib/filterMongo.ts
+++ lib/filterMongo.ts
@@ -3,8 +3,25 @@
  */
 import type { HydratedDocument } from "mongoose";
 
+function _filterMongo(entry: any): any {
+    const keys = Object.keys(entry);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]!;
+        if (key == "_id" || key == "__v") {
+            entry[key] = undefined;
+            delete entry[key];
+            continue;
+        }
+
+        if (typeof entry[key] === "object") {
+            entry[key] = _filterMongo(entry[key]);
+        }
+    }
+    return entry;
+}
+
 export function filterMongo(mongoResult: HydratedDocument<any>[]): any[] {
-    return mongoResult.map((entry: HydratedDocument<any>): any => {
-        return entry;
+    return mongoResult.map((entry) => {
+        return _filterMongo(entry.toObject());
     });
 }
