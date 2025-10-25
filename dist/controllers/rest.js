7534c102c27855728fe9edcfe816c6ca6c571f04
Troll321 <atila.ghulwani@gmail.com>
2025-10-25 08:11:53 +0700

    Removing Dist and Ignoreing dist

--- dist/controllers/rest.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { summariesModel } from "../schemas/models/summaries.js";
-import { filterMongo } from "../lib/filterMongo.js";
-const restRouter = Router();
-restRouter.get("/summary", async (req, res) => {
-    const summary = await summariesModel.find().limit(7);
-    res.json(filterMongo(summary));
-});
-export { restRouter };
