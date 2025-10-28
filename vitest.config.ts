1f6807923980d20ff17dcc895517f087056fd819
Troll321 <atila.ghulwani@gmail.com>
2025-10-28 12:32:24 +0700

    Configuring Vitest (Sequential Test Case)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,33 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { BaseSequencer, TestSpecification, Vitest, type TestSequencer } from "vitest/node";
+
+const testFiles = ["rest_v1.ts", "io_v1.ts"];
+
+class MySequencer extends BaseSequencer {
+    protected ctx: Vitest;
+
+    constructor(ctx: Vitest) {
+        super(ctx);
+        this.ctx = ctx;
+    }
+
+    async sort(specs: TestSpecification[]) {
+        return specs.sort((a: TestSpecification, b: TestSpecification) => {
+            const res =
+                testFiles.indexOf(a.moduleId.split("/").at(-1)!) -
+                testFiles.indexOf(b.moduleId.split("/").at(-1)!);
+            return res == 0 ? 0 : res < 0 ? -1 : 1;
+        });
+    }
+}
 
 export default defineConfig({
     test: {
         include: ["./tests/*.ts"],
+        fileParallelism: false,
+        sequence: {
+            sequencer: MySequencer,
+        },
     },
 });
