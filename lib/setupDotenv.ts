259c65d1e6036fe0bcd544acb2be8c0ea242a9b3
Troll321 <atila.ghulwani@gmail.com>
2025-12-25 10:14:07 +0700

    Adjust npm scripts & Changing .env logic

--- lib/setupDotenv.ts
+++ lib/setupDotenv.ts
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
+import fs from "fs";
 if (process.env.NODE_ENV !== "production") {
+    console.log("Running on development!");
     dotenv.config({ path: "./.d.env" });
+} else {
+    console.log("Running on preoduction!");
+    if (fs.existsSync("./.env")) {
+        dotenv.config({ path: "./.env" });
+    } else {
+        console.error("File .env doesn't exist");
+    }
 }
 
 export {};
