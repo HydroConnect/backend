ad12c66257eb5f1e20dbc3a2e220a2550d8510df
Troll321 <atila.ghulwani@gmail.com>
2025-12-25 10:26:40 +0700

    Fixing setupDotenv.ts

--- lib/setupDotenv.ts
+++ lib/setupDotenv.ts
@@ -4,11 +4,12 @@ if (process.env.NODE_ENV !== "production") {
     console.log("Running on development!");
     dotenv.config({ path: "./.d.env" });
 } else {
-    console.log("Running on preoduction!");
+    console.log("Running on production!");
     if (fs.existsSync("./.env")) {
         dotenv.config({ path: "./.env" });
     } else {
         console.error("File .env doesn't exist");
+        process.exit(1);
     }
 }
 
